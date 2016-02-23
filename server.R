cf130ecea27545914427bfe3c74bfdefe9b372f4
Alasdair Liam Hazen <alasdair.hazen@gmail.com>
2016-02-22 22:03:05 -0500

    Publication Final
    HTML with shiny app program inside

--- server.R
+++ server.R
@@ -6,9 +6,6 @@ shinyServer(
 
     output$newWeight <- renderText({input$idealWeight})
     output$activityLabel <- renderText({input$activity})
-    #output$activity <- renderText(paste("Feed", (round(((input$idealWeight * 70)^(3/4))) * {(as.numeric(input$activity))}), "calories for", input$age, "month old dog weighing", input$weight, "pounds. So feed", (input$kcal) ,"cups of your current food each day."))
     output$activity <- renderText(paste((round(((input$idealWeight * 70)^(3/4))) * {(as.numeric(input$activity))}) , "calories are required per day for a", input$age, "month old dog weighing", input$weight, "pounds. So feed", round((((input$idealWeight * 70)^(3/4))) * {(as.numeric(input$activity))} / input$kcal) ,"cups of your current dog food each day."))
-
-    #output$`This appliction is A product of SquirlDar` <- (renderText(((190 * 70)^(3/4)) * 3))
   }
 )
